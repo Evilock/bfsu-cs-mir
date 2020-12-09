596d56d7ae19f22d60261b36e1d1b9f0226fc409
Evilock <52896640+Evilock@users.noreply.github.com>
2020-12-09 09:03:50 +0800

    common css 1209

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -8,18 +8,8 @@ function getUserData()
             url: "/index",
             type: "post",
             dataType:"html",
-            success: function (data) {
-                data = JSON.stringify(data);
-                console.log(data);
-                if (Object.keys(data)[0]!=='uinfo') {
-                    var content2 = '<ul><li><text style="color: white">Hello~ </text></li>' +
-                        '<li><text style="color: white" th:utext="${uinfo.getNickName()}"></text></li></ul>';
-                    $("#melo").html(content2);
-                } else {
-                    var content = '<ul><li><a href="/login">登录</a></li>'+
-                        '<li><a href="/register">注册</a></li></ul>';
-                    $("#melo").html(content);
-                }
+            success: function () {
+                console.log("todo");
             },
             error: function () {
             },
